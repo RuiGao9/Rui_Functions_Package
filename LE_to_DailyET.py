a844298d03e8a6a785596c26c73bbc7174323e6d
Rui Gao <rui.gao@aggiemail.usu.edu>
2021-01-24 23:58:15 -0700

    Create LE_to_DailyET.py
    Create a function to calculate the daily ET based on instantent LE. Containing 3 approaches: EF, Rs, Rn-Rs ratio.

---
+++ LE_to_DailyET.py
@@ -0,0 +1,29 @@
+def LE_to_DailyET(LE,Approach='Rs',
+                  Rsi=500,Rsd=500,
+                  Rni=500,Rnd=500,
+                  Gi=500,Gd=500,
+                  c=1000,rou=997,lamb=2259.36):
+    '''
+    parameter LE: the instantent latent heat flux in W/m2.
+    parameter Approach: this one is string and it is restricted in 'Rs', 'EF', and 'Rn-Rs'.
+    parameter Rsi: the instantent solar radiation in W/m2.
+    parameter Rsd: the summation of the instantent solar radiation in W/m2.
+    parameter Rni: the instantent net radiation in W/m2.
+    parameter Rnd: the summation of the instantent net radiation in W/m2.
+    parameter Gi: the instantent soil heat flux in W/m2.
+    parameter Gd: the summation of the instantent soil heat flux in W/m2.
+    parameter c: a factor equal to 1000 converting meter to millimeter.
+    parameter rou: the water density in kg/m3.
+    parameter lamb: the water vaporization in MJ/kg. The heat of vaporization of water is around 540 cal/g(https://www.google.com/search?q=the+heat+of+vaporization+of+water&rlz=1C1CHBF_enUS932US932&oq=the+heat+of+vaporization+of+water&aqs=chrome..69i57j0l6j0i395l3.6550j1j4&sourceid=chrome&ie=UTF-8), which is around 2259.36MJ/kg.
+    
+    '''
+    if Approach=='Rs':
+        ETd = LE/Rsi*Rsd*c/(rou*lamb)
+    elif Approach=='EF':
+        ETd = LE/(Rni-Gi)*(Rnd-Gd)*c/(rou*lamb)
+    elif Approach=='Rn-Rs':
+        ETd = LE/(Rni-Gi)*Rni/Rsi*Rsd*c/(rou*lamb)
+    else:
+        print('Please check the inputs.')
+    
+    return ETd
