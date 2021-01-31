48ef0c40f05c506014879f2198a2e29c15c600f6
Rui Gao <rui.gao@aggiemail.usu.edu>
2021-01-31 16:10:55 -0700

    Create TellTheGeoInfo.py
    A function to tell the dimension of the image, and the geographic information and projection function.

---
+++ TellTheGeoInfo.py
@@ -0,0 +1,17 @@
+def TellTheGeoInfo(InputRaster):
+    '''
+    parameter InputRaster: input image
+    return: 1) dimension of the image, 2) geographic information, 3) projection information.
+    '''
+    import arcpy
+    import gdal
+    
+    fid=gdal.Open(dir_output+"\\"+Flight_Date+"_clip.tif")
+    input_img=fid.GetRasterBand(1).ReadAsArray()
+    dims=input_img.shape
+    print("Dimension of the data is:",dims[0],dims[1])
+    # Read the GDAL GeoTransform to get the pixel size
+    img_geo=fid.GetGeoTransform()
+    img_prj=fid.GetProjection()
+    fid=None
+    return(dims,img_geo,img_prj)
