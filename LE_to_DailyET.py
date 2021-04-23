d657085e30e0c3223709addb978b94d8704a9075
Rui Gao <rui.gao@aggiemail.usu.edu>
2021-04-23 13:19:17 -0600

    Update LE_to_DailyET.py
    Units about the equation are modified. Now, the Solar radiation approach, at least, is verified.

--- LE_to_DailyET.py
+++ LE_to_DailyET.py
@@ -1,28 +1,29 @@
 def LE_to_DailyET(LE,Approach='Rs',
-                  Rsi=500,Rsd=500,
-                  Rni=500,Rnd=500,
-                  Gi=500,Gd=500,
-                  c=1000,rou=997,lamb=2259.36):
+                  Rsi=800,Rsd=8000,
+                  Rni=500,Rnd=8000,
+                  Gi=500,Gd=4000,
+                  c=3600,rou=1000,lamb=2259.36):
+
     '''
-    parameter LE: the instantent latent heat flux in W/m2.
+    parameter LE: the instantent latent heat flux in W/m^2.
     parameter Approach: this one is string and it is restricted in 'Rs', 'EF', and 'Rn-Rs'.
     
-    parameter Rsi: the instantent solar radiation in W/m2.
-    parameter Rsd: the summation of the instantent solar radiation in W/m2.
-    parameter Rni: the instantent net radiation in W/m2.
-    parameter Rnd: the summation of the instantent net radiation in W/m2.
-    parameter Gi: the instantent soil heat flux in W/m2.
-    parameter Gd: the summation of the instantent soil heat flux in W/m2.
+    parameter Rsi: the instantent solar radiation in W/m^2.
+    parameter Rsd: the summation of the instantent solar radiation in W/m^2.
+    parameter Rni: the instantent net radiation in W/m^2.
+    parameter Rnd: the summation of the instantent net radiation in W/m^2.
+    parameter Gi: the instantent soil heat flux in W/m^2.
+    parameter Gd: the summation of the instantent soil heat flux in W/m^2.
     
-    parameter c: a factor equal to 1000 converting meter to millimeter.
-    parameter rou: the water density in kg/m3.
-    parameter lamb: the water vaporization in MJ/kg. The heat of vaporization of water is around 540 cal/g(https://www.google.com/search?q=the+heat+of+vaporization+of+water&rlz=1C1CHBF_enUS932US932&oq=the+heat+of+vaporization+of+water&aqs=chrome..69i57j0l6j0i395l3.6550j1j4&sourceid=chrome&ie=UTF-8), which is around 2259.36MJ/kg.
+    parameter c: a factor equal to 3600 converting second level to hour level.
+    parameter rou: the water density in kg/m^3.
+    parameter lamb: the water vaporization in 2259.36 kJ/kg
     
     '''
     ratio = c/(rou*lamb)
     # Solar radiation approach
     if Approach=='Rs':
-        ETd = LE/Rsi*Rsd*ratio
+        ETd = (LE/Rsi)*Rsd*ratio
     # Evaporative fraction approach
     elif Approach=='EF':
         ETd = LE/(Rni-Gi)*(Rnd-Gd)*ratio
