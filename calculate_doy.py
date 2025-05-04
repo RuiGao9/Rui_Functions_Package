3de091a578dba926ee2fddd92fa115657df94607
Rui Gao <rui.ray.gao@gmail.com>
2025-05-03 21:38:33 -0700

    Update calculate_doy.py

--- calculate_doy.py
+++ calculate_doy.py
@@ -3,6 +3,8 @@ def calculate_doy(year, month, day):
     Calculate DOY based on year, month, and day.
     
     '''
+    from datetime import datetime
+    
     date_object = datetime(year, month, day)
     doy = date_object.timetuple().tm_yday
 
