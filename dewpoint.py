643b96859d08ba2cda05212fff44f33506f03d11
Rui Gao <rui.ray.gao@gmail.com>
2025-05-13 13:29:02 -0700

    dew point calculation

---
+++ dewpoint.py
@@ -0,0 +1,19 @@
+def dewpoint(rh,temp):
+    '''
+    This function is built for dew point calculation.
+    
+    Parameters:
+    rh: relative humidity, from sensor (%)
+    temp: corresponding temperature
+
+    Return:
+    Dew point in degree C will be returned.
+    '''
+	import math
+	
+	a = 17.27
+	b = 237.7
+	parmeter = a*temp/(b+temp)+math.log(rh/100)
+	td = b*parameter/(a-parameter)
+	
+return(td)
